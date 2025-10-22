7043f8ac51856ba1f38b60f030166058d81211c8
ABawden2 <baway008@mymail.unisa.edu.au>
2025-10-22 17:35:24 +1100

    Changed the DjPad to have create the pattern option button group.

--- src/components/DjPad.js
+++ src/components/DjPad.js
@@ -1,10 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import CheckBox from './CheckBox';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import RadioButton from './RadioButton';
 
 function DjPad(props) {
   return (
     <Container>
+        <Row>
+            Pattern Options
+            <ButtonGroup>
+                {Object.values(props.patternOptions).map((pattern) => {
+                    return (
+                    <RadioButton
+                        key = {pattern.buttonId}
+                        buttonId = {pattern.buttonId}
+                        buttonName = {pattern.buttonName}
+                        buttonValue = {pattern.buttonValue}
+                        buttonGroupName = {pattern.buttonGroupName}
+                        buttonColour = {pattern.buttonColour}
+                        callBack = {pattern.callBack}
+                    />
+                )})}
+            </ButtonGroup>
+        </Row>
         <Row className={'g-' + props.rowGap}>
         {Object.values(props.checkBoxList).map((button) => {
             return (
@@ -25,3 +44,21 @@ function DjPad(props) {
 }
 
 export default DjPad;
+
+
+{/* <ButtonGroup>
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup> */}
