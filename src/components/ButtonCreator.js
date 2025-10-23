a8a5c3878674e2a1e0c94779e4763ccab44499bf
ABawden2 <baway008@mymail.unisa.edu.au>
2025-10-23 21:39:52 +1100

    Created a new component element, reformatted the buttonCreator component and changed the callback value in the buttonlist

--- src/components/ButtonCreator.js
+++ src/components/ButtonCreator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 
@@ -6,7 +6,22 @@ import Col from 'react-bootstrap/Col';
 function ButtonCreator (props) {
     return (
         <Col xs={12} md={props.buttonCols} lg={props.buttonCols}>
-            <Button variant="outline-success" type={props.buttonType} value={props.buttonName} onClick={props.callBack} style={{ width: 100 + '%' }}>{props.buttonName}</Button>
+            <Button 
+                variant="outline-success" 
+                id={props.buttonId}
+                type={props.buttonType} 
+                value={props.buttonName} 
+                onClick={(e) =>  {
+                    // props.callBack();
+                    // if (props.callBack === 'globalEditor.evaluate()') {
+                    //     console.log(props.callBack)
+                    //     props.callBack
+                    // }
+                    }}
+                style={{ width: 100 + '%' }}
+            >
+                {props.buttonName}
+            </Button>
         </Col>
     )
 }
